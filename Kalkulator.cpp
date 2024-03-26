d6f8ef634ded71c9c599ef18e9017ea068edba04
vararais <varadigmaisnann@gmail.com>
2024-03-26 17:38:33 +0700

    Menambah Void Input

---
+++ Kalkulator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input() {
+    cout << "Masukan Bilangan Pertama = ";
+    cin >> bilA;
+    cout << "Masukan Bilangan Kedua = ";
+    cin >> bilB;
+
+}
+
